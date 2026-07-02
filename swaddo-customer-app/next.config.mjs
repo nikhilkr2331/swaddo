eb6ea3f183423ac7d2f19ef22ab48797572d8e97
nikhilkr2331 <nikhil.kumar709198@gmail.com>
2026-07-02 07:43:51 +0530

    Configure vercel.json outputDirectory to out

--- swaddo-customer-app/next.config.mjs
+++ swaddo-customer-app/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Disabled for Vercel deployment
+  output: 'export',
   images: {
     unoptimized: true
   },
